dd6aeb875ff9ab42a3b7c279b200d1186e92a5b1
reinhek <75389881+reinhek@users.noreply.github.com>
2020-12-12 22:38:00 +0800

    Update day12.rs

--- day12.rs
+++ day12.rs
@@ -8,12 +8,12 @@ fn generator(input: &str) -> Vec<Action> {
             let integer = (&l[1..]).parse().unwrap();
 
             match action {
-                "N" => Action::North(integer),
-                "E" => Action::East(integer),
-                "W" => Action::West(integer),
-                "S" => Action::South(integer),
-                "L" => Action::Left(integer as f64),
-                "R" => Action::Right(integer as f64),
+                "N" => Action::Move(0, integer),
+                "E" => Action::Move(integer, 0),
+                "W" => Action::Move(-integer, 0),
+                "S" => Action::Move(0, -integer),
+                "L" => Action::Rotate(integer as f64),
+                "R" => Action::Rotate(-integer as f64),
                 "F" => Action::Forward(integer),
                 _=> panic!("Error {:?}", action),
             }
@@ -23,104 +23,91 @@ fn generator(input: &str) -> Vec<Action> {
 struct Ship {
     x: isize,
     y: isize,
+    waypoint_x: isize,
+    waypoint_y: isize,
     deg: f64,
 }
 
 impl Ship {
+
+    fn execute(&mut self, action: Action, part: i32) {
+        match action {
+            Action::Move(x,y) => {
+                match part {
+                    1 => {
+                        self.x += x;
+                        self.y += y;
+                    },
+                    2 => {
+                        self.waypoint_x += x;
+                        self.waypoint_y += y;
+                    },
+                    _=> panic!("Error")
+                }
+            },
+            Action::Rotate(d) => {
+                match part {
+                    1 => self.deg += d,
+                    2 => self.rotate(d),
+                    _=> panic!("Error")
+                }
+            },
+            Action::Forward(d) => {
+                match part {
+                    1 => {
+                        self.x += (d as f64 * self.deg.to_radians().cos()) as isize;
+                        self.y += (d as f64 * self.deg.to_radians().sin()) as isize;
+                    },
+                    2 => {
+                        self.x += d * self.waypoint_x;
+                        self.y += d * self.waypoint_y;
+                    },
+                    _=> panic!("Error")
+                }
+            },
+        }
+    }
+
     fn rotate(&mut self, degrees: f64) {
-        let angle = (self.y as f64/self.x as f64).atan();
+        let angle = (self.waypoint_y as f64/self.waypoint_x as f64).atan();
         let mut hyp = 0.0;
 
         if angle.sin() == 0.0 {
-            hyp = self.x as f64;
+            hyp = self.waypoint_x as f64;
         } else {
-            hyp = (self.y as f64 / angle.sin());
+            hyp = (self.waypoint_y as f64 / angle.sin());
         }
 
-        self.x = ((angle + degrees.to_radians()).cos() * hyp).round() as isize;
-        self.y = ((angle + degrees.to_radians()).sin() * hyp).round() as isize;
+        self.waypoint_x = ((angle + degrees.to_radians()).cos() * hyp).round() as isize;
+        self.waypoint_y = ((angle + degrees.to_radians()).sin() * hyp).round() as isize;
     }
 
-    fn project_course(&mut self, navigation: &Vec<Action>) -> isize {
+    fn project_course(&mut self, navigation: &Vec<Action>, part: i32) -> isize {
         for action in navigation {
-            execute(self, *action);
+            self.execute(*action, part);
         }
         self.x.abs() + self.y.abs()
-    }
-
-    fn project_course_2(&mut self, navigation: &Vec<Action>, waypoint: &mut Ship) -> isize {
-        for action in navigation {
-            match action {
-                Action::Forward(x) => {
-                    self.x += (*x as isize) * waypoint.x;
-                    self.y += (*x as isize) * waypoint.y;
-                },
-                Action::Left(x) => {
-                    waypoint.rotate(*x);
-                },
-                Action::Right(x) => {
-                    waypoint.rotate(-*x);
-                },
-                _=> {
-                    execute(waypoint, *action);
-                }
-            }
-
-        }
-        self.x.abs() + self.y.abs()
-    }
-}
-
-fn execute(object: &mut Ship, action: Action) {
-    match action {
-        Action::North(d) => {
-            object.y += d as isize;
-        },
-        Action::East(d) => {
-            object.x += d as isize;
-        },
-        Action::West(d) => {
-            object.x -= d as isize;
-        },
-        Action::South(d) => {
-            object.y -= d as isize;
-        },
-        Action::Left(d) => {
-            object.deg += d;
-        },
-        Action::Right(d) => {
-            object.deg -= d;
-        },
-        Action::Forward(d) => {
-            object.x += (d as f64 * object.deg.to_radians().cos()) as isize;
-            object.y += (d as f64 * object.deg.to_radians().sin()) as isize;
-        },
     }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Action {
-    North(i32),
-    South(i32),
-    East(i32),
-    West(i32),
-    Left(f64),
-    Right(f64),
-    Forward(i32),
+    Move(isize, isize),
+    Rotate(f64),
+    Forward(isize),
 }
 
 
 #[aoc(day12, part1)]
 fn solve_part1(input: &Vec<Action>) -> isize {
-    let mut ship = Ship { x: 0, y: 0, deg: 0.0};
-    ship.project_course(input)
+    let mut ship = Ship { x: 0, y: 0, waypoint_x: 0, waypoint_y: 0, deg: 0.0};
+    ship.project_course(input, 1)
 }
 
 #[aoc(day12, part2)]
 fn solve_part2(input: &Vec<Action>) -> isize {
-    let mut ship = Ship { x: 0, y: 0, deg: 0.0};
-    let mut waypoint = Ship {x: 10, y: 1, deg: 0.0};
-    ship.project_course_2(input, &mut waypoint)
+    let mut ship = Ship { x: 0, y: 0, waypoint_x: 10, waypoint_y: 1, deg: 0.0};
+    ship.project_course(input, 2)
 }
 
 #[cfg(test)]
